011879fc8d285d406f9d6710a2afc9bab76de238
Jeremy <jeremymvenegas@gmail.com>
2015-08-04 15:50:41 -0700

    Site updated to 00416c9

--- source/javascripts/global.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  $('#toggleMenu').click(function(){
-  	$(this).toggleClass('active');
-  	$('nav').toggleClass('active');
-  	$('nav .container > ul').toggleClass('active');
-  });
-});
-
